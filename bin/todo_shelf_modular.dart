e49ef5460e823512a1dee07aafdd0038e6d25d04
Orlando Eduardo Pereira <orlando-eduardo@hotmail.com>
2022-07-19 13:13:50 -0300

    cors middleware

--- bin/todo_shelf_modular.dart
+++ bin/todo_shelf_modular.dart
@@ -1,4 +1,6 @@
-import 'dart:developer';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
+import 'dart:io';
+import 'package:shelf/shelf.dart' as shelf;
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
@@ -10,8 +12,28 @@ Future<void> main(List<String> arguments) async {
   await ReadDotEnv().init();
   final env = ReadDotEnv().env;
 
+  const defaultHeadersList = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'no_authorization',
+    'refreshed_token',
+  ];
+
+  final overrideHeaders = {
+    ACCESS_CONTROL_ALLOW_HEADERS: defaultHeadersList.join(','),
+  };
+
   final server = await io.serve(
-    Modular(module: AppModule(), middlewares: [logRequests()]),
+    Modular(module: AppModule(), middlewares: [
+      logRequests(),
+      corsHeaders(headers: overrideHeaders),
+      jsonEncoder(),
+    ]),
     '0.0.0.0',
     int.parse(env['API_PORT'] ?? '3001'),
   );
@@ -27,4 +49,21 @@ Future<void> main(List<String> arguments) async {
   // );
 
   print('Server started: ${server.address.address}:${server.port}');
+}
+
+shelf.Middleware jsonEncoder() {
+  return (innerHanddler) {
+    return (request) async {
+      var response = await innerHanddler(request);
+
+      if (!response.headers.containsKey(HttpHeaders.contentTypeHeader)) {
+        response = response.change(headers: {
+          ...response.headers,
+          HttpHeaders.contentTypeHeader: 'application/json',
+        });
+      }
+
+      return response;
+    };
+  };
 }
